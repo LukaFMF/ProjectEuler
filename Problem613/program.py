50890ab7ba4c1975959def7aabc02812fe3246f1
Luka Toplak <luka.toplak@student.fmf.uni-lj.si>
2019-11-06 13:54:35 +0100

    Popravek

--- Problem613/program.py
+++ Problem613/program.py
@@ -11,11 +11,37 @@
 #probability that the ant leaves the triangle along its longest side?
 #Give your answer rounded to 10 digits after the decimal point.
 
-import math
+import math,random
 
 dolzinaA = 3
 dolzinaB = 4
 dolzinaC = 5
 
-gamma = math.pi/2
+#tocke v koordinatnem sistemu
+tockaA= (0,dolzinaA)
+tockaB = (dolzinaB,0)
+tockaC = (0,0)
 
+alpha = math.asin(dolzinaA/dolzinaC)
+beta = math.asin(dolzinaB/dolzinaC)
+gamma = math.pi/2 #pravi kot, nasproti hipotenuzi
+
+stevilo_ponovitev = 10
+stevec = 0
+verjetnost = 0
+while stevec < stevilo_ponovitev:
+    kotC = oklepajociKot(tockaA,tockaB,tockaC)
+    verjetnost += kotC/2*math.pi
+    stevec += 1
+
+verjetnost = round(verjetnost/stevilo_ponovitev,10)
+print(verjetnost)
+
+
+def oklepajociKot(tockaA,tockaB,tockaC):
+    ''' Vrne kot z vrhom v nakljucni tocki znotraj trikotnika, katerega nasprotna stranice je hipotenuza trikotnika ''' 
+    nakljucniX = random.uniform(0,1) * tockaA[1] #nakljucna koordinata na stranici b 
+    maxYvTockiX = -(tockaA[1]/tockaB[0]) * nakljucniX + tockaA[1] #enacba premice skozi tocki A in B v katero vstavimo x 
+    nakljucniY = random.uniform(0,1) * maxYvTockiX #nakljucna koordinata na stranici a, ki pa je znotraj trikotnika 
+
+    nakljucnaTocka = nakljucniX,nakljucniY #sestavimo tocko
